7e1c329555b4eea9cae1480a1590b9f115241a2a
BBKite <bb.kite@outlook.com>
2023-03-17 20:54:52 +0000

    Generate lower case alphabet using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+		puthchar('\n');
+	return (0);
+}
