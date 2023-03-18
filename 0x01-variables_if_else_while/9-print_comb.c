cec515358c38973692b83e2993fb6646c5178b56
BBKite <bb.kite@outlook.com>
2023-03-18 08:54:36 +0000

    Print all possible combinations of single-digit numbers with putchar function

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,17 @@
 */
 int main(void)
 {
-	char alf;
+	int n;
 
-	for (alf = 'z'; alf >= 'a'; alf--)
-	putchar(alf);
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
-
 	return (0);
 }
