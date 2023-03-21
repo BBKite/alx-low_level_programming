5ecf8c34a393100405a23664127b4a7ab55d9078
BBKite <bb.kite@outlook.com>
2023-03-21 23:28:28 +0000

    10 Returns the result of two added integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - returns the result of two added integers
+* @x: formal parameter that has value passed to it
+* @y: formal parameter that has value passed to it
+* Return: added result of x and y
+*/
+int add(int x, int y)
+{
+return (x + y);
+}
