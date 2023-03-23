2533bd07bd518d7a8815ab24b77ded88f27737e6
BBKite <bb.kite@outlook.com>
2023-03-23 21:29:18 +0000

    Multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+* @a: formal parameter that has integer value passed to it
+* @b: fromal parameter that has integer value passed to it
+* Return: result of multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
