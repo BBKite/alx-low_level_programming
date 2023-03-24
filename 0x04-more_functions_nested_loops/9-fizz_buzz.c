64b7703e803f627812ec31247a7e0ea5f90793db
BBKite <bb.kite@outlook.com>
2023-03-24 17:49:57 +0000

    Fixed space and new line requirement

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,23 @@
 
 int main(void)
 {
-int count;
+	int count;
 
-for (count = 1; count <= 100; count++)
-	{
-	if (count % 15 == 0)
-		printf("FizzBuzz ");
-	else if (count % 5 == 0)
-		printf("Buzz ");
-	else if (count % 3 == 0)
-		printf("Fizz ");
-	else
-	printf("%d ", count);
-	}
+	for (count = 1; count <= 100; count++)
+		{
+		if (count % 15 == 0)
+			printf("FizzBuzz");
+		else if (count % 5 == 0)
+			printf("Buzz");
+		else if (count % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", count);
 
-printf("\n");
-
-return (0);
+		if (count != 100)
+			printf(" ");
+		else
+			printf("\n");
+		}
+	return (0);
 }
