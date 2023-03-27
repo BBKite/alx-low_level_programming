116a0c4807987fa9cfcfefddc5867829b14e04cb
BBKite <bb.kite@outlook.com>
2023-03-27 23:58:23 +0000

    0 Takes a pointer to an int parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes pointer to int parameter and updates
+* the value it points to to 98
+* @n: value at address used to update paramter
+* return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
