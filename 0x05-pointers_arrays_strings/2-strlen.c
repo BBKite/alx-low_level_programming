836c6030ba163d15a1fe122bae65dd816d85698a
BBKite <bb.kite@outlook.com>
2023-03-28 00:02:13 +0000

    2 Returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: points to the string outside of the function
+* Return: i, the iteration count of the string's characters
+*/
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; i <= s[i]; i++)
+	continue;
+
+return (i);
+}
