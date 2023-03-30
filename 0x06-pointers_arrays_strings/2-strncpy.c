a6d8ba9f2ad76aae082ca8af18ed24ba6be39bec
BBKite <bb.kite@outlook.com>
2023-03-30 18:18:28 +0000

    2 Copies a string, similar to strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string, like strncpy function
+* @src: string being read from
+* @dest: resulting destination string
+* @n: terminating null byte
+* Return: dest to buffer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+
+return (dest);
+}
